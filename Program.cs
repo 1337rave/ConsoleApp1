01417d4703aa641f2ca4e21d08cd05f2ab6c1da7
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-20 20:04:01 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the value of A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the value of B (greater than A): ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = A; i <= B; i++)
+        {
+            // Repeat the current number i times
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
