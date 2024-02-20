7dca94e75600e89ccfaef9b59f8b0b6db305ff3d
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2024-02-20 20:00:57 +0200

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a six-digit number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        // Reverse the number
+        int reversedNumber = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Reversed number: {reversedNumber}");
+    }
+}
